c3e8525b672c6e8d93dcf93550a861e487014561
kacper kostecki <k.kostecki99@gmail.com>
2023-01-06 16:16:56 +0100

    optimized db initial script

--- tass_project/flights/models.py
+++ tass_project/flights/models.py
@@ -1,20 +1,15 @@
 from django.db import models
 
 
-class OrginAirport(models.Model):
+class Airport(models.Model):
     code = models.PositiveIntegerField(unique=True,null=False, blank=False)
     description = models.CharField(max_length=100)
 
-class DestAirport(models.Model):
-    code = models.PositiveIntegerField(unique=True, null=False, blank=False)
-    description = models.CharField(max_length=100)
-
-
-class Flights(models.Model):
+class Flight(models.Model):
     itin_id = models.PositiveBigIntegerField(null=False, blank=False)
     mkt_id = models.PositiveBigIntegerField(null=False, blank=False)
     quarter = models.PositiveIntegerField(null=False, blank=False)
-    origin_airport_id = models.ForeignKey(OrginAirport, to_field="code", on_delete=models.CASCADE)
-    dest_airport_id = models.ForeignKey(DestAirport, to_field="code", on_delete=models.CASCADE)
+    origin_airport = models.ForeignKey(Airport, to_field="code", on_delete=models.CASCADE, related_name="origin_airport")
+    dest_airport = models.ForeignKey(Airport, to_field="code", on_delete=models.CASCADE, related_name="destination_airport")
     passengers = models.PositiveIntegerField(null=False, blank=False)
     distance = models.PositiveIntegerField(null=False, blank=False)
--- tass_project/initial_db.py
+++ tass_project/initial_db.py
@@ -1,32 +1,47 @@
 import django
+from tqdm import tqdm
 django.setup()
 
 import csv
 from django.db import transaction
 
-from flights.models import DestAirport, OrginAirport, Flights
+from flights.models import Airport, Flight
 
-with transaction.atomic():
-    with open("./data/L_AIRPORT_ID.csv", "r") as file:
-        rf = csv.DictReader(file, fieldnames=["code","description"])
-        for i, record in enumerate(rf):
-            if i ==0:
-                continue 
-            DestAirport.objects.create(code=int(record["code"]),description=record["description"])
-            OrginAirport.objects.create(code=int(record["code"]),description=record["description"])
 
-with transaction.atomic():
-    with open("./data/flights.csv", "r") as file:
-        rf = csv.DictReader(file, fieldnames=["id", "ItinID", "MktID", "Year", "Quarter", "OriginAirportID", "DestAirportID", "Passengers", "MktDistance"])
-        for i, record in enumerate(rf):
-            if i ==0:
-                continue 
-            Flights.objects.create(
-                itin_id=record["ItinID"],
-                mkt_id=record["MktID"],
-                quarter=record["Quarter"],
-                origin_airport_id=OrginAirport.objects.get(code=int(record["OriginAirportID"])),
-                dest_airport_id =DestAirport.objects.get(code=int(record["DestAirportID"])),
-                passengers=int(float(record["Passengers"])),
-                distance=int(float(record["MktDistance"])),
-                )
+print("import airports")
+with open("./data/L_AIRPORT_ID.csv", "r") as file:
+    rf = csv.DictReader(file, fieldnames=["code","description"])
+    airports = []
+    for i, record in tqdm(enumerate(rf)):
+        if i ==0:
+            continue
+        airports.append(Airport(code=int(record["code"]),description=record["description"]))
+    with transaction.atomic():
+        Airport.objects.bulk_create(airports)
+print("import airports done")
+
+
+limit_bulk = 1000000 # create partially because of memory
+print("import flights")
+with open("./data/flights.csv", "r") as file:
+    rf = csv.DictReader(file, fieldnames=["id", "ItinID", "MktID", "Year", "Quarter","OriginAirportID",
+                                            "DestAirportID", "Passengers", "MktDistance"])
+    airports = {airport.code: airport for airport in Airport.objects.all()}
+    flights = []
+    for i, record in tqdm(enumerate(rf)):
+        if i == 0:
+            continue
+        flights.append(Flight(
+            itin_id=record["ItinID"],
+            mkt_id=record["MktID"],
+            quarter=record["Quarter"],
+            origin_airport=airports[int(record["OriginAirportID"])],
+            dest_airport=airports[int(record["OriginAirportID"])],
+            passengers=int(float(record["Passengers"])),
+            distance=int(float(record["MktDistance"])),
+        ))
+        if len(flights) == limit_bulk:
+            Flight.objects.bulk_create(flights)
+            print(f"Created {i} records")
+            flights = []
+print("import flights done")
